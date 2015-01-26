b17c9123addd72e83b655df97675061b0e49fec4
Chuck <ucichuck@gmail.com>
2015-01-25 16:46:46 -0800

    Chuck: add solution for Anagrams

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,48 @@ package leetcode.Solutions;
 
 
 //import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.LinkedList;
+//
+//
+//public class Solution {
+//    public  String sortChars(String s) {
+//        char[] content = s.toCharArray();
+//        Arrays.sort(content);
+//        return new String(content);
+//    }   
+//
+//    public ArrayList<String> anagrams(String[] strs) {
+//        ArrayList<String> res = new ArrayList<String>();
+//        HashMap<String, LinkedList<String>> hash = new HashMap<String, LinkedList<String>>();
+//     
+//        /* Group words by anagram */
+//        for (String s : strs) {
+//            String key = sortChars(s); 
+//            if (!hash.containsKey(key)) {
+//                hash.put(key, new LinkedList<String>());
+//            }   
+//            LinkedList<String> anagrams = hash.get(key);
+//            anagrams.push(s);
+//        } 
+//        for (String key : hash.keySet()) {
+//            LinkedList<String> list = hash.get(key);
+//            if (list.size() > 1) {
+//// should be bigger than 1.  1 means no anagrams
+//                for (String t : list) {
+//                    res.add(t);
+//                }   
+//            }
+//        }   
+//        return res;
+//    }
+//}
+
+
+
+
+//import java.util.ArrayList;
 //import java.util.HashSet;
 //import java.util.List;
 //
