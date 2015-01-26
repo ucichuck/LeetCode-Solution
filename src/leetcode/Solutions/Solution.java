30347ff5538adf647593cab98eddeb091c2ffb1e
ucichuck <ucichuck@gmail.com>
2015-01-25 23:22:18 -0800

    chuck:  add solution for Reverse Nodes in k-Group

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,55 @@ package leetcode.Solutions;
 
 
 //public class Solution {
+//    public ListNode reverseKGroup(ListNode head, int k) {
+//        if(head == null || head.next == null || k == 1){
+//        	return head;
+//        }
+//        
+//       int size = 0;
+//       ListNode cur = head;
+//       while(cur != null){
+//    	   cur = cur.next;
+//    	   size ++;
+//       }
+//       
+//       int loop = size / k;
+//       cur = head;
+//       ListNode res = new ListNode(-1);
+//       res.next = head;
+//       ListNode pre = res;
+//      
+//       
+//       for(int i=0; i<loop; i++){
+//    	   int j = 0;
+//    	   ListNode post = cur.next;
+//    	   while(post != null){
+//    		   ListNode next = post.next;
+//    		   post.next = cur;
+//    		   cur = post;
+//    		   post = next;
+//    		   j++;
+//    		   if(j == k-1){
+//    			   ListNode tmp = pre.next;
+//    			   pre.next = cur;
+//    			   tmp.next = post;
+//    			   pre = tmp;
+//    			   cur = pre.next;
+//    			   break;
+//    		   }
+//    	   }
+//       }
+//       
+//       return res.next;
+//        
+//    }
+//}
+
+
+
+
+
+//public class Solution {
 //    public boolean canJump(int[] A) {
 //        if(A == null || A.length == 0){
 //        	throw new IllegalArgumentException("wrong input");
