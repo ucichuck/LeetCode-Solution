b5837b5c2d5ff36fbc8c49c75b409ab3760d377e
ucichuck <ucichuck@gmail.com>
2015-01-27 00:01:33 -0800

    Best Time to Buy and Sell Stock II

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -8,6 +8,25 @@ package leetcode.Solutions;
 
 //public class Solution {
 //    public int maxProfit(int[] prices) {
+//    	if(prices == null || prices.length <2){
+//        	return 0;
+//        }
+//    	int maxp = 0;
+//    	for(int i=1; i<prices.length; i++){
+//    		if(prices[i] > prices[i-1]){
+//    			maxp+=prices[i]-prices[i-1];
+//    		}
+//    	}
+//    	return maxp;
+//    }
+//}
+
+
+
+
+
+//public class Solution {
+//    public int maxProfit(int[] prices) {
 //        if(prices == null || prices.length <2){
 //        	return 0;
 //        }
