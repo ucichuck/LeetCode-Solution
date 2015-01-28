e72c620f0a057d6de404cbd5c08a14a14246abbe
ucichuck <ucichuck@gmail.com>
2015-01-27 21:55:40 -0800

    CHUCK: Binary Tree Maximum Path Sum

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,38 @@ package leetcode.Solutions;
 
 
 //public class Solution {
+//    public int maxPathSum(TreeNode root) {
+//        // Start typing your Java solution below
+//        // DO NOT write main() function
+//       int[] res = maxPathSums(root);
+//        return res[1];
+//    }
+//    
+//    public int[] maxPathSums(TreeNode root){
+//        int[] res = new int[2];
+//        if(root==null){
+//            res[0]=Integer.MIN_VALUE; // Must be min value since it will add root.val;
+//            res[1]=Integer.MIN_VALUE;
+//            return res;
+//        }
+//        int[] fromLeft = maxPathSums(root.left),
+//              fromRight = maxPathSums(root.right);
+//        
+//        int temp1 = fromLeft[0]>0?(fromLeft[0]+root.val):root.val;
+//        int temp2 = fromRight[0]>0?(fromRight[0]+root.val):root.val;
+//        
+//        res[0]=Math.max(temp1,temp2);
+//        res[1]=Math.max(fromLeft[1],Math.max(fromRight[1], Math.max(temp1+temp2-root.val,res[0]))); //double root.val and if fromLeft or fromRightis negative will not be added.
+//        
+//        return res;
+//    }
+//}
+
+
+
+
+
+//public class Solution {
 //    public boolean isPalindrome(String s) {
 //    	if(s == null || s.isEmpty()){
 //    		return true;
