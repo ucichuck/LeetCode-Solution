fcd49d655efcaa4c60f07928b5256e1b7c81a916
ucichuck <ucichuck@gmail.com>
2015-01-27 22:19:28 -0800

    chuck: Sum Root to Leaf Numbers

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,44 @@ package leetcode.Solutions;
 
 
 
+//import java.util.ArrayList;
+//
+//import javax.swing.tree.TreeNode;
+//
+//public class Solution {
+//    public int sumNumbers(TreeNode root) {
+//        if(root == null){
+//        	return 0;
+//        }
+//        
+//        ArrayList<Integer> res = new ArrayList<Integer>();
+//        cal(root, 0, res);
+//        int sum = 0;
+//        for(Integer tmp : res){
+//        	sum += tmp;
+//        }
+//        return sum;
+//    }
+//    
+//    private void cal(TreeNode root, int sum, ArrayList<Integer> res){
+//    	if(root.left == null && root.right == null){
+//    		res.add(sum*10+root.val);
+//    	}
+//    	
+//    	if(root.left != null){
+//    		cal(root.left,sum*10+root.val, res);
+//    	}
+//    	
+//    	if(root.right != null){
+//    		cal(root.right,sum*10+root.val, res);
+//    	}
+//    }
+//}
+
+
+
+
+
 //import java.util.HashSet;
 //
 //public class Solution {
---
+++ src/leetcode/Solutions/SumRoottoLeafNumbers.java
@@ -0,0 +1,35 @@
+package leetcode.Solutions;
+
+import java.util.ArrayList;
+
+import javax.swing.tree.TreeNode;
+
+public class SumRoottoLeafNumbers {
+	 public int sumNumbers(TreeNode root) {
+	        if(root == null){
+	        	return 0;
+	        }
+	        
+	        ArrayList<Integer> res = new ArrayList<Integer>();
+	        cal(root, 0, res);
+	        int sum = 0;
+	        for(Integer tmp : res){
+	        	sum += tmp;
+	        }
+	        return sum;
+	    }
+	    
+	    private void cal(TreeNode root, int sum, ArrayList<Integer> res){
+	    	if(root.left == null && root.right == null){
+	    		res.add(sum*10+root.val);
+	    	}
+	    	
+	    	if(root.left != null){
+	    		cal(root.left,sum*10+root.val, res);
+	    	}
+	    	
+	    	if(root.right != null){
+	    		cal(root.right,sum*10+root.val, res);
+	    	}
+	    }
+}
