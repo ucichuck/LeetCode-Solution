7b3eb281b68e5485bbcb3cbb231330c36b304662
ucichuck <ucichuck@gmail.com>
2015-01-28 21:35:10 -0800

    single number 2

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -8,6 +8,28 @@ package leetcode.Solutions;
 
 //public class Solution {
 //    public int singleNumber(int[] A) {
+//      if(A == null || A.length == 0){
+//    	return 0;
+//    }
+//      int one = 0, two = 0, three = 0;
+//      for(int i=0; i<A.length; i++){
+//    	  two |= one & A[i];
+//    	  one ^= A[i];
+//    	  three = two & one;
+//    	  
+//    	  one &= ~three;
+//    	  two &= ~three;
+//      }
+//      return one;
+//    }
+//}
+
+
+
+
+
+//public class Solution {
+//    public int singleNumber(int[] A) {
 //        if(A == null || A.length == 0){
 //        	return 0;
 //        }
