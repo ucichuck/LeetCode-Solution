578b6cb0e786d3ebf2e74f1ac037ba9385629f41
ucichuck <ucichuck@gmail.com>
2015-01-31 22:07:32 -0800

    Unique Binary Search Trees

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,30 @@ package leetcode.Solutions;
 
 
 
+//public class Solution {
+//    public int numTrees(int n) {
+//    	if(n <= 1){
+//    		return n;
+//    	}
+//        int[] table = new int[n+1];
+//        table[0] = 1;
+//        table[1] = 1;
+//        for(int i=2; i<=n; i++){
+//        	int sum = 0;
+//        	for(int j=0; j<i; j++){
+//        		sum += table[i-j-1]*table[j];
+//        	}
+//        	table[i] = sum;
+//        }
+//        
+//        return table[n];
+//    }
+//}
+
+
+
+
+
 //import java.util.ArrayList;
 //import java.util.List;
 //import java.util.Stack;
