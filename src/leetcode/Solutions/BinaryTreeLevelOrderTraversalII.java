cfaa3bacca0365f32c18b5918bcd78c3f8ac4bed
ucichuck <ucichuck@gmail.com>
2015-02-01 13:54:51 -0800

    Binary Tree Level Order Traversal

---
+++ src/leetcode/Solutions/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,56 @@
+package leetcode.Solutions;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+import leetcode.Utility.TreeNode;
+
+public class BinaryTreeLevelOrderTraversalII {
+	public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> currLevel = new Stack<TreeNode>();
+        Stack<TreeNode> nextLevel = new Stack<TreeNode>();
+        Stack<TreeNode> tmp;
+        
+        currLevel.push(root);
+        boolean normalOrder = true;
+
+        while (!currLevel.isEmpty()) {
+            ArrayList<Integer> currLevelResult = new ArrayList<Integer>();
+
+            while (!currLevel.isEmpty()) {
+                TreeNode node = currLevel.pop();
+                currLevelResult.add(node.val);
+
+                if (normalOrder) {
+                    if (node.left != null) {
+                        nextLevel.push(node.left);
+                    }
+                    if (node.right != null) {
+                        nextLevel.push(node.right);
+                    }
+                } else {
+                    if (node.right != null) {
+                        nextLevel.push(node.right);
+                    }
+                    if (node.left != null) {
+                        nextLevel.push(node.left);
+                    }
+                }
+            }
+
+            result.add(currLevelResult);
+            tmp = currLevel;
+            currLevel = nextLevel;
+            nextLevel = tmp;
+            normalOrder = !normalOrder;
+        }
+
+        return result;
+
+    }
+}
--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,66 @@ package leetcode.Solutions;
 
 
 //import java.util.ArrayList;
+//import java.util.Stack;
+//
+//import leetcode.Utility.TreeNode;
+//
+//
+//public class Solution {
+//    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+//        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+//
+//        if (root == null) {
+//            return result;
+//        }
+//        Stack<TreeNode> currLevel = new Stack<TreeNode>();
+//        Stack<TreeNode> nextLevel = new Stack<TreeNode>();
+//        Stack<TreeNode> tmp;
+//        
+//        currLevel.push(root);
+//        boolean normalOrder = true;
+//
+//        while (!currLevel.isEmpty()) {
+//            ArrayList<Integer> currLevelResult = new ArrayList<Integer>();
+//
+//            while (!currLevel.isEmpty()) {
+//                TreeNode node = currLevel.pop();
+//                currLevelResult.add(node.val);
+//
+//                if (normalOrder) {
+//                    if (node.left != null) {
+//                        nextLevel.push(node.left);
+//                    }
+//                    if (node.right != null) {
+//                        nextLevel.push(node.right);
+//                    }
+//                } else {
+//                    if (node.right != null) {
+//                        nextLevel.push(node.right);
+//                    }
+//                    if (node.left != null) {
+//                        nextLevel.push(node.left);
+//                    }
+//                }
+//            }
+//
+//            result.add(currLevelResult);
+//            tmp = currLevel;
+//            currLevel = nextLevel;
+//            nextLevel = tmp;
+//            normalOrder = !normalOrder;
+//        }
+//
+//        return result;
+//
+//    }
+//}
+
+
+
+
+
+//import java.util.ArrayList;
 //import java.util.List;
 //
 //import leetcode.Utility.TreeNode;
