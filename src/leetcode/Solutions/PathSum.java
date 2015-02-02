81aa85580da0bfc55ee27b36fb3ccb366bd1f404
ucichuck <ucichuck@gmail.com>
2015-02-01 22:54:13 -0800

    Path Sum

---
+++ src/leetcode/Solutions/PathSum.java
@@ -0,0 +1,15 @@
+package leetcode.Solutions;
+
+import leetcode.Utility.TreeNode;
+
+public class PathSum {
+	 public boolean hasPathSum(TreeNode root, int sum) {
+	        if(root == null){
+	        	return false;
+	        }
+	        if(sum == root.val && (root.left == null) && (root.right == null)){
+	        	return true;
+	        }       
+	        	return hasPathSum(root.left , sum - root.val) || hasPathSum(root.right, sum-root.val);
+	    }
+}
--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -9,6 +9,24 @@ package leetcode.Solutions;
 //import leetcode.Utility.TreeNode;
 //
 //public class Solution {
+//    public boolean hasPathSum(TreeNode root, int sum) {
+//        if(root == null){
+//        	return false;
+//        }
+//        if(sum == root.val && (root.left == null) && (root.right == null)){
+//        	return true;
+//        }       
+//        	return hasPathSum(root.left , sum - root.val) || hasPathSum(root.right, sum-root.val);
+//    }
+//}
+
+
+
+
+
+//import leetcode.Utility.TreeNode;
+//
+//public class Solution {
 //    public int minDepth(TreeNode root) {
 //        if(root == null){
 //        	return 0;
