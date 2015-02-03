ea0e1ba34f1f273e888f1c2c885c05a79ed9bb7d
ucichuck <ucichuck@gmail.com>
2015-02-02 22:59:24 -0800

    Triangle

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,36 @@ package leetcode.Solutions;
 
 
 
+//import java.util.List;
+//
+//public class Solution {
+//    public int minimumTotal(List<List<Integer>> triangle) {
+//        if(triangle == null || triangle.size() == 0){
+//        	return 0;
+//        }
+//        int len = triangle.size();
+//        
+//        int[] arr = new int[len];
+//        
+//        for(int i= len-1; i>=0; i--){
+//        	if(i==len-1){
+//        		for(int j=0; j<triangle.get(i).size(); j++){
+//        			arr[j] = triangle.get(i).get(j);
+//        		}
+//        	}else{
+//        		for(int j=0; j<triangle.get(i).size(); j++){
+//        			arr[j] = Math.min(arr[j], arr[j+1])+triangle.get(i).get(j);
+//        		}
+//        	}
+//        }
+//        return arr[0];
+//    }
+//}
+
+
+
+
+
 //import java.util.ArrayList;
 //import java.util.List;
 //
