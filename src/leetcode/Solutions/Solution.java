494fb5f2ea3b70a61114bc91e7d87c29dd384bd8
ucichuck <ucichuck@gmail.com>
2015-02-02 23:13:12 -0800

    Merge Intervals

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,54 @@ package leetcode.Solutions;
 
 
 
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.Comparator;
+//import java.util.List;
+//
+//import leetcode.Utility.Interval;
+//
+//public class Solution {
+//    public List<Interval> merge(List<Interval> intervals) {
+//        if(intervals == null ||intervals.size()== 0){
+//        	return intervals;
+//        }
+//        
+//        Collections.sort(intervals, new TmpComparator());
+//        List<Interval> res = new ArrayList<Interval>();
+//        Interval cur = intervals.get(0);
+//        for(int i=1; i<intervals.size(); i++ ){
+//        	Interval tmp = intervals.get(i);
+//        	if(cur.end < tmp.start){
+//        		res.add(cur);
+//        		cur = tmp;
+//        	}else{
+//        		cur.end = Math.max(cur.end, tmp.end);
+//        	}
+//        }
+//        res.add(cur);
+//        return res;
+//    }
+//    private class TmpComparator implements Comparator<Interval>{
+//
+//		@Override
+//		public int compare(Interval t1, Interval t2) {
+//			if(t1.start < t2.start){
+//				return -1;
+//			}else if(t1.start == t2.start){
+//				return 0;
+//			}else{
+//				return 1;
+//			}
+//		}
+//    	
+//    }
+//}
+
+
+
+
+
 //import java.util.List;
 //
 //public class Solution {
