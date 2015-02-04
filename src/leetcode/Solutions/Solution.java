bfc2b7c62936c822e30b6161a251bae33ba294c7
ucichuck <ucichuck@gmail.com>
2015-02-03 22:35:58 -0800

    Evaluate Reverse Polish Notation

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,44 @@ package leetcode.Solutions;
 
 
 
+//import java.util.Stack;
+//
+//public class Solution {
+//    public int evalRPN(String[] tokens) {
+//        if(tokens == null || tokens.length == 0){
+//        	return 0;
+//        }
+//        
+//        Stack<Integer> st = new Stack<Integer>();
+//        for( int i=0; i<tokens.length; i++){
+//        	if(!isFac(tokens[i])){
+//        		st.push(Integer.valueOf(tokens[i]));
+//        	}else{
+//        		int b = st.pop();
+//        		int a = st.pop();
+//        		st.push(cal(a,b,tokens[i]));		
+//        	}
+//        }
+//        return st.pop();
+//    }
+//    private boolean isFac(String st){
+//    	return st.equals("*")||st.equals("/")||st.equals("+")||st.equals("-");
+//    }
+//    private int cal(int a, int b, String st){
+//    	switch(st){
+//    	case "+": return a+b;
+//    	case "-": return a-b;
+//    	case "*" : return a*b;
+//    	default : return a/b;
+//    	}
+//    		
+//    }
+//}
+
+
+
+
+
 //import leetcode.Utility.ListNode;
 //
 //public class Solution {
