4c6fe957d5e33f40f0ba9ad1a526f7edc60aa48c
ucichuck <ucichuck@gmail.com>
2015-02-04 21:22:02 -0800

    Spiral Matrix II

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,59 @@ package leetcode.Solutions;
 
 
 //public class Solution {
+//    public int[][] generateMatrix(int n) {
+//        if(n <= 0){
+//        	return null;
+//        }
+//        int[][] res = new int[n][n];
+//        int count = 1;
+//        int k = n;
+//        int l = n;
+//        int x = 0;
+//        int y = 0;
+//        while(k >0 && l > 0){
+//        	if(k == 1){
+//        		for(int i=0; i<l; i++){
+//        			res[x][y++] = count++;
+//        		}
+//        		break;
+//        	}else if(l == 1){
+//        		for(int i =0; i<k; i++){
+//        			res[x++][y] = count++;
+//        		}
+//        		break;
+//        	}
+//        	
+//        	for(int i=0; i<l-1; i++){
+//        		res[x][y++] = count++;
+//        	}
+//        	
+//        	for(int i=0; i<k-1; i++){
+//        		res[x++][y] = count++;
+//        	}
+//        	
+//        	for(int i=0; i<l-1; i++){
+//        		res[x][y--] = count++;
+//        	}
+//        	
+//        	for(int i=0; i<k-1; i++){
+//        		res[x--][y] = count++;
+//        	}
+//        	
+//        	x++;
+//        	y++;
+//        	k = k-2;
+//        	l = l-2;
+//        }
+//        return res;
+//    }
+//}
+
+
+
+
+
+//public class Solution {
 //    public void rotate(int[][] matrix) {
 //        if(matrix == null || matrix.length == 0){
 //        	return ;
