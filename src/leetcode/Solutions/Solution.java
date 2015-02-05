d34335b8634e8d7841c1e95a6f7d64384fb64882
ucichuck <ucichuck@gmail.com>
2015-02-04 21:13:05 -0800

    Rotate Image

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,30 @@ package leetcode.Solutions;
 
 
 //public class Solution {
+//    public void rotate(int[][] matrix) {
+//        if(matrix == null || matrix.length == 0){
+//        	return ;
+//        }
+//        int n = matrix.length;
+//        int k = n%2 == 0? n/2 : n/2+1;
+//        for(int i=0; i<n/2; i++){
+//        	for(int j=0; j<k; j++){
+//        		int tmp = matrix[i][j];
+//        		matrix[i][j] = matrix[n-1-j][i];
+//        		matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+//        		matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+//        		matrix[j][n-1-i] = tmp;
+//        	}
+//        }
+//        return ;
+//    }
+//}
+
+
+
+
+
+//public class Solution {
 //    public int[] twoSum(int[] numbers, int target) {
 //        int[] res = new int[2];
 //        int left = 0;
