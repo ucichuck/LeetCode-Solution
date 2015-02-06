cc06c4337c3774447ac9b00a444e52a7aec1f289
ucichuck <ucichuck@gmail.com>
2015-02-05 22:05:34 -0800

    Remove Duplicates from Sorted Array II

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,44 @@ package leetcode.Solutions;
 
 
 //public class Solution {
+//    public int removeDuplicates(int[] A) {
+//        // Start typing your Java solution below
+//        // DO NOT write main() function
+//        int len =A.length;
+//        if(len==1){
+//            return 1;
+//        }else if(len<=0){
+//            return 0;
+//        }
+//        int count=1;
+//        int cur=A[0];
+//        int b=1;
+//        int resLen =0;
+//        for(int i=1; i<len; i++){
+//            if(cur==A[i]){
+//                if(count<2){
+//                    count++;
+//                    A[b]=A[i];
+//                    b++;
+//                }
+//            }else{
+//                A[b]=A[i];
+//                cur=A[i];
+//                b++;
+//                count=1;
+//            }
+//        }
+//        
+//        return b;
+//        
+//    }
+//}
+
+
+
+
+
+//public class Solution {
 //
 //    public int divide(int dividend, int divisor) {
 //        if(dividend == Integer.MIN_VALUE && divisor == -1){
