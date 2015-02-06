c5cab8eddd2808f1bed5a675e8d5cc7ef12cb6fb
ucichuck <ucichuck@gmail.com>
2015-02-05 22:14:40 -0800

    Binary Tree Preorder Traversal

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,39 @@ package leetcode.Solutions;
 
 
 
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Stack;
+//
+//import leetcode.Utility.TreeNode;
+//
+//public class Solution {
+//    public List<Integer> preorderTraversal(TreeNode root) {
+//    	List<Integer> res = new ArrayList<Integer>();
+//    	if(root == null){
+//    		return res;
+//    	}
+//    	
+//    	Stack<TreeNode> st = new Stack<TreeNode>();
+//    	st.add(root);
+//    	while(!st.isEmpty()){
+//    		TreeNode cur = st.pop();
+//    		res.add(cur.val);
+//    		if(cur.right != null){
+//    			st.add(cur.right);
+//    		}
+//    		if(cur.left != null){
+//    			st.add(cur.left);
+//    		}
+//    	}
+//    	return res;
+//    }
+//}
+
+
+
+
+
 //public class Solution {
 //    public int removeDuplicates(int[] A) {
 //        // Start typing your Java solution below
