d32c15205e2e8227d9f73f2a9aaffc41f85c6581
ucichuck <ucichuck@gmail.com>
2015-02-06 21:55:29 -0800

    Intersection of Two Linked Lists

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -1,17 +1,43 @@
 package leetcode.Solutions;
 
-import leetcode.Utility.ListNode;
 
-public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA == null || headB == null){
-        	return null;
-        }
-    	
-    	int count = 0;
-        
-    }
-}
+
+
+
+
+
+//import leetcode.Utility.ListNode;
+//
+//public class Solution {
+//    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//        if(headA == null || headB == null){
+//        	return null;
+//        }
+//        ListNode pA = headA;
+//        ListNode pB = headB;
+//        while(pA != null && pB != null){
+//        	pA = pA.next;
+//        	pB = pB.next;
+//        }
+//        
+//        while(pA != null){
+//        	pA = pA.next;
+//        	headA = headA.next;
+//        }
+//        
+//        while(pB != null){
+//        	pB = pB.next;
+//        	headB = headB.next;
+//        }
+//        
+//    	while(headA != headB){
+//    		headA= headA.next;
+//    		headB = headB.next;
+//    	}
+//    	return headA;
+//        
+//    }
+//}
 
 
 
