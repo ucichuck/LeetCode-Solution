e3a7a5bee590d88f4109b4c4dc3d6099567567ff
ucichuck <ucichuck@gmail.com>
2015-02-07 18:40:52 -0800

    Roman to Integer

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,39 @@ package leetcode.Solutions;
 
 
 
+//import java.util.HashMap;
+//
+//public class Solution {
+//    public int romanToInt(String s) {
+//        HashMap<Character, Integer> table = new HashMap<Character,Integer>();
+//        table.put('I', 1);
+//        table.put('V', 5);
+//        table.put('X', 10);
+//        table.put('L', 50);
+//        table.put('C', 100);
+//        table.put('D', 500);
+//        table.put('M', 1000);
+//        
+//        if(s == null || s.length() == 0){
+//        	return 0;
+//        }
+//        int pre = 0; int res = 0;
+//        for(int i=0; i<s.length(); i++){
+//        	int cur = table.get(s.charAt(i));
+//        	if(cur <= pre){
+//        		res+=cur;
+//        	}else{
+//        		res += cur - pre*2;
+//        	}
+//        	pre = cur;
+//        }
+//        return res;
+//    }
+//}
+
+
+
+
 //import java.util.Arrays;
 //import java.util.Comparator;
 //
