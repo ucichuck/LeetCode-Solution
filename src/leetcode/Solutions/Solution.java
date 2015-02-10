c717f964b4b2e8c6b495eb3f2b798d348e96e46f
ucichuck <ucichuck@gmail.com>
2015-02-09 23:17:07 -0800

    edit distance

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,35 @@ package leetcode.Solutions;
 
 
 //public class Solution {
+//    public int minDistance(String word1, String word2) {
+//        int[][] arr = new int[word1.length()+1][word2.length()+1];
+//        
+//        for(int i=0; i<=word1.length(); i++){
+//        	for(int j=0; j<=word2.length(); j++){
+//        		if(i == 0 && j == 0){
+//        			arr[i][j] = 0;
+//        		}else if(i == 0){
+//        			arr[i][j] = arr[i][j-1]+1;
+//        		}else if(j == 0){
+//        			arr[i][j] = arr[i-1][j]+1;
+//        		}else{
+//        			if(word1.charAt(i-1) == word2.charAt(j-1)){
+//        				arr[i][j] = arr[i-1][j-1];
+//        			}else{
+//        				arr[i][j] = Math.min(arr[i-1][j-1], Math.min(arr[i][j-1], arr[i-1][j]))+1;
+//        			}
+//        		}
+//        	}
+//        }
+//        return arr[word1.length()][word2.length()];
+//    }
+//}
+
+
+
+
+
+//public class Solution {
 //    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 //        if(obstacleGrid.length == 0 || obstacleGrid[0].length == 0){
 //        	return 0;
