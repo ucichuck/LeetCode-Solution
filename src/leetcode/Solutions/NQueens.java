ce66854c10bb7fab0dd98c7b1b60ca90dc429a00
ucichuck <ucichuck@gmail.com>
2015-02-09 22:46:12 -0800

    N-Queens

--- src/leetcode/Solutions/NQueens.java
+++ src/leetcode/Solutions/NQueens.java
@@ -5,68 +5,55 @@ import java.util.Arrays;
 import java.util.List;
 
 public class NQueens {
-	public List<String[]> solveNQueens(int n) {
-    	List<String[]> res = new ArrayList<String[]>();
-        if(n <= 0){
-        	return res;
-        }
-        
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-        	arr[i] = -1;
-        }
-        
-        List<int[]> boards = new ArrayList<int[]>();
-        
-        cal(0, n ,arr,boards);
-        
-        for(int[] a : boards){
-        	String[] sss = new String[n];
-        	for(int j=0; j<a.length; j++){
-        		String tmp = "";
-        		for(int i=0; i<n; i++){
-            		if(i == a[j]){
-            			tmp+="Q";
-            		}else{
-            			tmp+=".";
-            		}
-            	}
-        		sss[j] = tmp;
-        	}
-        	res.add(sss);
-        }
-        return res;
-    }
-    private void cal(int index, int n, int[] arr , List<int[]> boards){
-    	if(n == index){
-    		int[] arr2 = Arrays.copyOfRange(arr, 0, arr.length);
-    		boards.add(arr2);
-    		return; // bug: remember to Write return, otherwise use if else!!!!!
-    	}
-    	
-    	for(int i=0; i<n; i++){
-    		arr[index] = i;
-    		if(isValid(arr, index)){
-    			cal(index+1, n, arr, boards);
-    		}
-    		arr[index] = -1;
-    	}
-    }
-    
-    private boolean isValid(int[] arr, int index){
-    	if(index == 0){
-    		return true;
-    	}
-    	
-    	for(int i=0; i<index; i++){
-    		if(arr[i] == arr[index]){
-    			return false;
-    		}
-    		
-    		if(Math.abs(i-index) == Math.abs(arr[i] - arr[index])){
-    			return false;
-    		}
-    	}
-    	return true;
-    }
+	 public List<String[]> solveNQueens(int n) {
+	    	List<String[]> arr = new ArrayList<String[]>();
+	    	if(n <= 0){
+	    		return arr;
+	    	}
+	    	
+	    	int[] board = new int[n];
+	    	
+	    	cal(board, arr,0,n);
+	    	return arr;
+	    }
+	    
+	    private void cal(int[] board, List<String[]> arr, int index, int n){
+	    	if(index == n){
+	    		String[] tmp = new String[n];
+	    		for(int i=0; i<n; i++){
+	    			tmp[i] = getString(board[i],n);
+	    		}
+	    		arr.add(tmp);
+	    		return;
+	    	}
+	    	
+	    	for(int i=0; i<n; i++){
+	    		board[index] = i;
+	    		if(isValid(board,index)){
+	    			cal(board,arr,index+1,n);
+	    		}
+	    		board[index] = 0;
+	    	}
+	    }
+	    
+	    private boolean isValid(int[] board, int index){
+	    	for(int i=0; i< index; i++){
+	    		if(board[i] == board[index] || Math.abs(board[i]-board[index]) == Math.abs(i-index) ){
+	    			return false;
+	    		}
+	    	}
+	    	return true;
+	    }
+	    
+	    private String getString(int index, int n){
+	    	String res = "";
+	    	for(int i=0; i<n; i++){
+	    		if(i == index){
+	    			res+="Q";
+	    		}else{
+	    			res+=".";
+	    		}
+	    	}
+	    	return res;
+	    }
 }
--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,67 @@ package leetcode.Solutions;
 
 
 
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Solution {
+//    public List<String[]> solveNQueens(int n) {
+//    	List<String[]> arr = new ArrayList<String[]>();
+//    	if(n <= 0){
+//    		return arr;
+//    	}
+//    	
+//    	int[] board = new int[n];
+//    	
+//    	cal(board, arr,0,n);
+//    	return arr;
+//    }
+//    
+//    private void cal(int[] board, List<String[]> arr, int index, int n){
+//    	if(index == n){
+//    		String[] tmp = new String[n];
+//    		for(int i=0; i<n; i++){
+//    			tmp[i] = getString(board[i],n);
+//    		}
+//    		arr.add(tmp);
+//    		return;
+//    	}
+//    	
+//    	for(int i=0; i<n; i++){
+//    		board[index] = i;
+//    		if(isValid(board,index)){
+//    			cal(board,arr,index+1,n);
+//    		}
+//    		board[index] = 0;
+//    	}
+//    }
+//    
+//    private boolean isValid(int[] board, int index){
+//    	for(int i=0; i< index; i++){
+//    		if(board[i] == board[index] || Math.abs(board[i]-board[index]) == Math.abs(i-index) ){
+//    			return false;
+//    		}
+//    	}
+//    	return true;
+//    }
+//    
+//    private String getString(int index, int n){
+//    	String res = "";
+//    	for(int i=0; i<n; i++){
+//    		if(i == index){
+//    			res+="Q";
+//    		}else{
+//    			res+=".";
+//    		}
+//    	}
+//    	return res;
+//    }
+//}
+
+
+
+
+
 //import java.util.HashMap;
 //
 //public class Solution {
