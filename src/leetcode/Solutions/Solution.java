15cd81e9cb1ea17c19ed28f8043fa51733ce2371
ucichuck <ucichuck@gmail.com>
2015-02-10 23:51:41 -0800

    Valid Palindrome

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,52 @@ package leetcode.Solutions;
 
 
 
+//public class Solution {
+//    public boolean isPalindrome(String s) {
+//        if(s.length() <=1){
+//        	return true;
+//        }
+//        
+//        s = s.toLowerCase();
+//        
+//        int left = 0;
+//        int right = s.length()-1;
+//        
+//        while(left < right){
+//        	if(!isAlphaNemric(s.charAt(left))){
+//        		left++;
+//        		continue;
+//        	}
+//        	
+//        	if(!isAlphaNemric(s.charAt(right))){
+//        		right--;
+//        		continue;
+//        	}
+//        	
+//        	if(s.charAt(left) != s.charAt(right)){
+//        		return false;
+//        	}
+//        	left ++;
+//        	right --;
+//        }
+//        return true;
+//    }
+//    
+//    private boolean isAlphaNemric(char cur){
+//    	if(cur >= 'a' && cur <= 'z' ){
+//    		return true;
+//    	}
+//    	if(cur >= '0' && cur <= '9'){
+//    		return true;
+//    	}
+//    	return false;
+//    }
+//}
+
+
+
+
+
 //import java.util.ArrayList;
 //import java.util.List;
 //
