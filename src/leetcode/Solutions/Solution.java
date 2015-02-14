9f2186f89e98091ac89d103b198de9e3deadd786
ucichuck <ucichuck@gmail.com>
2015-02-13 22:24:00 -0800

    Search Insert Position

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,34 @@ package leetcode.Solutions;
 
 
 //public class Solution {
+//    public int searchInsert(int[] A, int target) {
+//        if(A == null || A.length == 0){
+//        	return -1;
+//        }
+//        
+//        int left = 0;
+//        int right = A.length-1;
+//        while(left <= right){
+//        	int mid = (left+right)/2;
+//        	
+//        	if(A[mid] == target){
+//        		return mid+1;
+//        	}else if(A[mid] < target){
+//        		left = mid+1;
+//        	}else{
+//        		right = mid-1;
+//        	}
+//        	
+//        }
+//        return left;
+//    }
+//}
+
+
+
+
+
+//public class Solution {
 //    public int divide(int dividend, int divisor) {
 //        // Start typing your Java solution below
 //        // DO NOT write main() function
