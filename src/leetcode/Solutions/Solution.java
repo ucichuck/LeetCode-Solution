3f4d01a3dcf80ae8299be916db57a0f620228a1b
ucichuck <ucichuck@gmail.com>
2015-02-13 22:36:26 -0800

    First Missing Positive

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -7,6 +7,38 @@ package leetcode.Solutions;
 
 
 //public class Solution {
+//    public int firstMissingPositive(int[] A) {
+//        if(A == null ||A.length == 0){
+//        	return 1;
+//        }
+//        
+//        for(int i=1; i<=A.length; ){
+//        	if(A[i-1] == i){
+//        		i++;
+//        		continue;
+//        	}else{
+//        		int index = A[i-1];
+//        		if(index >0 && index <=A.length && A[index-1] != index){
+//        			int tmp = A[index-1];
+//        			A[index-1] = index;
+//        			A[i-1] = tmp;
+//        		}
+//        	}
+//        }
+//        for(int i=0; i<A.length; i++){
+//        	if(A[i] != i+1){
+//        		return i+1;
+//        	}
+//        }
+//        return A.length+1;
+//    }
+//}
+
+
+
+
+
+//public class Solution {
 //    public int searchInsert(int[] A, int target) {
 //        if(A == null || A.length == 0){
 //        	return -1;
