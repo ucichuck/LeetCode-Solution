00624d6fabf4860ebb7c4bf6b168a6af4b125684
ucichuck <ucichuck@gmail.com>
2015-02-20 00:05:02 -0800

    Factorial Trailing Zeroes

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,26 @@ package leetcode.Solutions;
 
 
 
+//public class FactorialTrailingZeroes {
+//	public int trailingZeroes(int n) {
+//        if(n< 4){
+//        	return 0;
+//        }
+//        
+//        int ret = 0;
+//        while(n>0)
+//        {
+//            ret += n/5;
+//            n /= 5;
+//        }
+//        return ret;
+//    }
+//}
+
+
+
+
+
 //public class Solution {
 //    public String convertToTitle(int n) {
 //        if(n <= 0){
