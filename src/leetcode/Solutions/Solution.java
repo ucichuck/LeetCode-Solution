134a9895ea54a0e52b418387fc7c5df9e50d3803
ucichuck <ucichuck@gmail.com>
2015-02-20 21:05:10 -0800

    Next Permutation

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,51 @@ package leetcode.Solutions;
 
 
 
+//public class Solution {
+//    public void nextPermutation(int[] num) {
+//        if(num.length<=1){
+//        	return;
+//        }
+//        
+//        int index = num.length-1;
+//        
+//        while(index > 0){
+//        	if(num[index-1] < num[index]){
+//        		break;
+//        	}
+//        	index --;
+//        }
+//        
+//       
+//        if(index > 0){
+//             index --;
+//        	int end = num.length-1;
+//        	while(index < end){
+//        		if(num[end] > num[index]){
+//        			int tmp = num[index];
+//        			num[index] = num[end];
+//        			num[end] = tmp;
+//        			index++;
+//        			break;
+//        		}
+//        		end--;
+//        	}
+//        }
+//        int end = num.length-1;
+//        while(index < end){
+//        	int tmp = num[end];
+//        	num[end] = num[index];
+//        	num[index] = tmp;
+//        	end--;
+//        	index ++;
+//        }
+//    }
+//}
+
+
+
+
+
 //public class FactorialTrailingZeroes {
 //	public int trailingZeroes(int n) {
 //        if(n< 4){
