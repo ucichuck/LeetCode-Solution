7649dcc2571a71767439566a015f2f208957a592
ucichuck <ucichuck@gmail.com>
2015-02-23 23:28:50 -0800

    Unique Paths II

--- src/leetcode/Solutions/Solution.java
+++ src/leetcode/Solutions/Solution.java
@@ -6,6 +6,44 @@ package leetcode.Solutions;
 
 
 
+//import java.util.List;
+//
+//public class Solution {
+//    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+//        if(obstacleGrid.length == 0 || obstacleGrid[0].length == 0){
+//        	return 0;
+//        }
+//        
+//        int x = obstacleGrid.length;
+//        int y = obstacleGrid[0].length;
+//        
+//        int[][] arr = new int[x][y];
+//        
+//        for(int i=0; i<x; i++){
+//        	for(int j=0; j<y; j++){
+//        		if(i == 0 & j == 0){
+//        			arr[i][j] = obstacleGrid[i][j] == 1 ? 0 : 1;
+//        		}else if(i == 0){
+//        			arr[i][j] = obstacleGrid[i][j] == 1? 0 : arr[i][j-1];
+//        		}else if(j == 0){
+//        			arr[i][j] = obstacleGrid[i][j] == 1? 0 : arr[i-1][j];
+//        		}else{
+//        			if(obstacleGrid[i][j] == 1){
+//        				arr[i][j] = 0;
+//        			}else{
+//        				arr[i][j] = arr[i-1][j] + arr[i][j-1];
+//        			}
+//        		}
+//        	}
+//        }
+//        return arr[x-1][y-1];
+//    }
+//}
+
+
+
+
+
 //import java.util.ArrayList;
 //import java.util.List;
 //
